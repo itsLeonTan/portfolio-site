b5c58d1864e02620888eb50b14aa41174d0bd20b
TannedLeon <101375873+TannedLeon@users.noreply.github.com>
2026-07-22 22:31:40 -0700

    Beta 3.2.1 - Nav-bar box shadow made permenant. - Isolated Nav-bar from landing section. - Changed nav-bar position from fixed to sticky.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,6 @@
 
 const nav = document.querySelector('nav');
 
-window.addEventListener('scroll', () => {
-    nav.classList.toggle('scrolled', window.scrollY > 0);
-});
-
 function menuFunction(x) {
     x.classList.toggle("change");
     document.querySelector(".menu-overlay").classList.toggle("active");
